e62e3af1675852a59cc85613fd713511b61bb0ef
ninankkim <69530297+ninankkim@users.noreply.github.com>
2020-08-20 22:16:38 -0400

    Add files via upload

---
+++ smallestnumber.py
@@ -0,0 +1,3 @@
+numbers = [5, 4, 3, 2, 1]
+numbers.sort()
+print ("The smallest number is:", *numbers[:1])
---
+++ sumthenumbers.py
@@ -0,0 +1,6 @@
+numbers = [1, 2, 3, 4, 5]
+sum = 0
+for number in numbers:
+    print (number)
+    sum = sum + number
+print (sum)
